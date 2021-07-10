af827ed7380e3d329b188a3d9eeeefb0d19161ad
Rakha Djokosoetono <48625405+rakhadjo@users.noreply.github.com>
2021-07-11 01:27:43 +0700

    Add analytics (#5)
    * npm installed analytics
    
    * add analytics id
    
    * working analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,20 @@ module.exports = {
     description: '',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-BL6Z5D5EYV', // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
