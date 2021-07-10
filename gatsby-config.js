413136e6ca4fffd1b29a97e0f26fcde27a235f67
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2021-07-11 01:26:55 +0700

    working analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,17 +5,25 @@ module.exports = {
     description: '',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-BL6Z5D5EYV', // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-offline`,
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        trackingId: 'UA-198108803-1', // Google Analytics / GA,
-      },
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
